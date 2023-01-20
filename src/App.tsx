0dd2ee2e6da96a855c1e7e84694cc5f638bd9eca
Bong <qhdgkdbs@gmail.com>
2023-01-20 21:47:02 +0900

    ✨ TodoList, TodoItem 컴포넌트 추가   - 기본적인 뼈대만 만듬

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 
 type Todo = {
@@ -7,7 +8,23 @@ type Todo = {
 };
 
 function App() {
-  return <div></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  return <TodoList todos={todos} />;
+}
+
+function TodoList({ todos }: { todos: Todo[] }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem todo={todo} />
+      ))}
+    </ul>
+  );
+}
+
+function TodoItem({ todo }: { todo: Todo }) {
+  return <li>{todo.title}</li>;
 }
 
 export default App;
